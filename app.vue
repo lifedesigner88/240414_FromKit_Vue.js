6c68468ed9a44c0639eb113edccc41ddce464865
sejongPark <lifedesigner88@gmail.com>
2024-04-14 10:27:40 +0900

    기본포멧

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <FormKit
+        label="Username"
+        name="username"
+        type="text"
+        help="Pick a new username"
+        validation="required|matches:/^@{a-zA-Z]+$/|length:5"
+        value="@useFromKit"
+        prefix-icon="avatarMan"
+    />
+    <FormKit type="text"/>
+    <FormKit type="select"/>
+    <FormKit type="textarea"/>
   </div>
 </template>
